b9f1e0b8a293d43ad073db6ad9f3244e9ff8302e
JackieShuman <jackie.shuman@gmail.com>
2017-01-06 13:07:05 -0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Compute inverse of square matrix
+## or retrieve previously calculated inverse from the cache
 
-## Write a short comment describing this function
 
+## store matrix and inverse matrix values
 makeCacheMatrix <- function(x = matrix()) {
-
+    #initialize objects
+    im<- NULL
+    #set asigns input argument to x in parent environ
+    #and assigns value of NULL to im bject in parent environ
+    set <- function(y) {
+        x <<- y
+        im <<- NULL
+    }
+    #get retrieves data for x
+    get <- function() x
+    #setinvmatrix assigns input argument "im" in parent environ
+    setinvmatrix <- function(invmatrix)im <<- invmatrix
+    #getinvmatrix retrieves inverse value "im"
+    getinvmatrix <- function () im
+    #assign values to list and put into parent environ
+    list(set = set, get = get,
+         setinvmatrix = setinvmatrix,
+         getinvmatrix = getinvmatrix)
 }
 
 
-## Write a short comment describing this function
-
+## Calclate inverse of matrix from makeCacheMatrix 
+## or retrieve from cache if previously calculated
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    
+    im <- x$getinvmatrix()
+    #check if inverse matrix value exists in cache
+    #if it does, print message and return value "im"
+    if(!is.null(im)){
+        message("getting cached data")
+        return(im)
+    }
+    #if no "im" value in cache then calculate inverse
+    #call get function to retrieve matrix data
+    data <-x$get()
+    ## Return a matrix that is the inverse of 'x'
+    im <- solve(data, ...)
+    #call setinvmatrix function to assign "im"value
+    x$setinvmatrix(im)
+    im
 }
